eb2d729d0f320a807e89e1a27982c34865096988
Telesporter <51327163+Telesporter@users.noreply.github.com>
2022-04-05 22:32:08 +0800

    Delete z_webfront/src/main/webapp directory

--- z_webfront/src/main/webapp/jsLearning/SquareCartoon/cartoon.js
+++
@@ -1,8 +0,0 @@
-$(function main() {
-    setInterval(function () {
-        let a = $(".s1");
-        let b = $(".s2");
-        a.removeClass("s1").addClass("s2");
-        b.removeClass("s2").addClass("s1");
-    },1000)
-});
--- z_webfront/src/main/webapp/jsLearning/jsLearn/jsLearn.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("#first").on("mouseover",function () {
-        $("#first").html("Teleporter");
-    });
-    $("#first").on("mouseout",function () {
-        $("#first").html("Zebra");
-    });
-});
