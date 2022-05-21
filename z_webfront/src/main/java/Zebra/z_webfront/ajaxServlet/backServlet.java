a68aff98e12ebeb3756f5de69bf8127c37b85add
Telesporter <51327163+Telesporter@users.noreply.github.com>
2022-05-21 22:39:19 +0800

    Add files via upload

---
+++ z_webfront/src/main/java/Zebra/z_webfront/ajaxServlet/backServlet.java
@@ -0,0 +1,28 @@
+package Zebra.z_webfront.ajaxServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2022/4/5
+ * Time: 19:10
+ * Description: No Description
+ */
+@WebServlet("/back")
+public class backServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println(frontServlet.count);
+    }
+}
---
+++ z_webfront/src/main/java/Zebra/z_webfront/ajaxServlet/frontServlet.java
@@ -0,0 +1,50 @@
+package Zebra.z_webfront.ajaxServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2022/4/5
+ * Time: 19:10
+ * Description: No Description
+ */
+
+@WebServlet("/front")
+public class frontServlet extends HttpServlet {
+    static int count = 0;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        count++;
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html");
+
+        String template = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>首页</title>\n" +
+                "    <script src=\"jsLearn/jquery-3.6.0.min.js\"></script>\n" +
+                "    <script src = \"AjaxLearn/CountViewTimes/front.js\"></script>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <p id = \"content\">::count::</p>\n" +
+                "</body>\n" +
+                "</html>";
+        String content = template.replace("::count::", String.valueOf(count));
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println(content);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
